e481d0907a681f20b11674c1667afa8d1e598e65
jeric realubit <jericrealubit@gmail.com>
2025-04-26 02:40:32 +1200

    graph

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -6,30 +6,56 @@ import { ref, watch } from 'vue'
 
 const selectedTextStore = useSelectedTextStore()
 const { selectedText } = storeToRefs(selectedTextStore)
-const data = ref(null)
-const error = ref(null)
+
+const meta_data = ref([])
+
+const option = ref({
+  title: { text: '', left: 'center' },
+  tooltip: { trigger: 'axis' },
+  xAxis: { type: 'category', data: [] },
+  yAxis: { type: 'value', scale: true },
+  series: [
+    {
+      name: 'Close',
+      type: 'line',
+      data: [],
+      smooth: true,
+      lineStyle: { width: 2 },
+    },
+  ],
+  dataZoom: [
+    { type: 'inside', start: 20, end: 100 },
+    { show: true, type: 'slider', top: '90%', start: 20, end: 100 },
+  ],
+})
 
 const fetchData = async (text) => {
   try {
-    //;[0]['Meta Data']['2. Symbol']
     // const response = await axios.get(`http://localhost:8000/stocks/${text}`)
     const response = await axios.get(`http://localhost:8000/stocks`)
-    console.log(response.data[0]['Meta Data']['2. Symbol'])
-    console.log(response.data[0]['Time Series (5min)'])
-    data.value = response.data
-    error.value = null
+
+    meta_data.value = response.data[0]['Meta Data']
+    console.log(meta_data.value)
+    console.log(meta_data.value['1. Information'])
+    const ts = response.data[0]['Time Series (5min)']
+    // sort datetimes ascending
+    const times = Object.keys(ts).sort()
+    const closes = times.map((t) => parseFloat(ts[t]['4. close']))
+
+    option.value.title = {
+      text: `${meta_data.value['2. Symbol']} 5-Min Close Price`,
+      left: 'center',
+    }
+    option.value.xAxis.data = times
+    option.value.series[0].data = closes
   } catch (err) {
-    error.value = err.message
-    data.value = null
+    console.error('Error Fetching Stocks Data', err)
   }
 }
 
 watch(selectedText, (newText) => {
   if (newText) {
     fetchData(newText)
-  } else {
-    data.value = null
-    error.value = null
   }
 })
 </script>
@@ -38,5 +64,16 @@ watch(selectedText, (newText) => {
   <main class="p-4 flex-1 overflow-y-auto">
     <h2 class="text-lg font-bold mb-4">Stock Informations</h2>
     <p>Clicked Text: {{ selectedTextStore.selectedText }}</p>
+    <!-- 
+    <div>{{ meta_data.value['1. Information'] }}</div>
+    <div>{{ meta_data.value['2. Symbol'] }}</div>
+    <div>{{ meta_data.value['3. Last Refreshed'] }}</div>
+    <div>{{ meta_data.value['4. Interval'] }}</div>
+    <div>{{ meta_data.value['5. Output Size'] }}</div>
+    <div>{{ meta_data.value['6. Time Zone'] }}</div> -->
+
+    <div class="w-full max-w-3xl mx-auto">
+      <v-chart :option="option" autoresize style="height: 400px" />
+    </div>
   </main>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,30 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import VChart from 'vue-echarts'
+import * as echarts from 'echarts/core'
+import { LineChart } from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DataZoomComponent,
+} from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+
+echarts.use([
+  LineChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DataZoomComponent,
+  CanvasRenderer,
+])
 
 const app = createApp(App)
 const pinia = createPinia()
+
+app.component('v-chart', VChart)
 
 app.use(router)
 app.use(pinia)
