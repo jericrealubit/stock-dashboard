d599c5e4401b69e67af882c0f9c874b2ac40d0cd
jeric realubit <jericrealubit@gmail.com>
2025-04-27 12:56:31 +1200

    working query api

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -7,6 +7,11 @@ import { ref, watch } from 'vue'
 const selectedTextStore = useSelectedTextStore()
 const { selectedText } = storeToRefs(selectedTextStore)
 
+//+ Select Timeframe
+const selected = ref('INTRADAY')
+const options = ref(['INTRADAY', 'DAILY', 'WEEKLY', 'MONTHLY'])
+//-
+
 const meta_data = ref([])
 
 const option = ref({
@@ -29,21 +34,39 @@ const option = ref({
   ],
 })
 
-const fetchData = async (text) => {
+const fetchData = async (query) => {
+  console.log('xxx', query)
+  const id = query.id
+  const timeframe = query.timeframe
+  console.log(id, ' === ', timeframe)
   try {
-    // const response = await axios.get(`http://localhost:8000/stocks/${text}`)
-    const response = await axios.get(`http://localhost:8000/stocks`)
+    const response = await axios.get(`http://localhost:8000/stocks`, {
+      params: query,
+    })
+    console.log(response.data)
+    //meta_data.value = response.data['Meta Data']
+    meta_data.value = Object.values(response.data)[0]
 
-    meta_data.value = response.data[0]['Meta Data']
     console.log(meta_data.value)
     console.log(meta_data.value['1. Information'])
-    const ts = response.data[0]['Time Series (5min)']
+
+    //const ts = response.data['Time Series (5min)']
+    const timeseries = Object.values(response.data)[1]
+    const ts = timeseries
+
+    // const response = await axios.get(`http://localhost:8000/stocks`, {
+    //   params: query,
+    // })
+    // console.log(response.data[1])
+    // meta_data.value = response.data[1]['Meta Data']
+    //const ts = response.data[1]['Time Series (5min)']
+
     // sort datetimes ascending
     const times = Object.keys(ts).sort()
     const closes = times.map((t) => parseFloat(ts[t]['4. close']))
 
     option.value.title = {
-      text: `${meta_data.value['2. Symbol']} 5-Min Close Price`,
+      text: `${Object.values(meta_data.value)[1]}: ${Object.values(meta_data.value)[0]} (Closing Price)`,
       left: 'center',
     }
     option.value.xAxis.data = times
@@ -53,9 +76,19 @@ const fetchData = async (text) => {
   }
 }
 
-watch(selectedText, (newText) => {
-  if (newText) {
-    fetchData(newText)
+// watch(selectedText, (newText, selected) => {
+//   if (newText || selected) {
+//     console.log({ id: newText, timeframe: selected })
+//     fetchData({ id: newText, timeframe: selected })
+//   }
+// })
+
+watch([selectedText, selected], ([newVar1, newVar2], [oldVar1, oldVar2]) => {
+  if (newVar1 || newVar2) {
+    // console.log(`var1 changed from ${oldVar1} to ${newVar1}`)
+    // console.log(`var2 changed from ${oldVar2} to ${newVar2}`)
+    // console.log({ id: newVar1, timeframe: newVar2 })
+    fetchData({ id: newVar1, timeframe: newVar2 })
   }
 })
 </script>
@@ -63,17 +96,39 @@ watch(selectedText, (newText) => {
 <template>
   <main class="p-4 flex-1 overflow-y-auto">
     <h2 class="text-lg font-bold mb-4">Stock Informations</h2>
-    <p>Clicked Text: {{ selectedTextStore.selectedText }}</p>
-    <!-- 
-    <div>{{ meta_data.value['1. Information'] }}</div>
-    <div>{{ meta_data.value['2. Symbol'] }}</div>
-    <div>{{ meta_data.value['3. Last Refreshed'] }}</div>
-    <div>{{ meta_data.value['4. Interval'] }}</div>
-    <div>{{ meta_data.value['5. Output Size'] }}</div>
-    <div>{{ meta_data.value['6. Time Zone'] }}</div> -->
+    <p class="font-bold">Selected Symbol: {{ selectedTextStore.selectedText }}</p>
+    <div>
+      <hr class="border-t border-gray-300 my-5" />
+    </div>
 
-    <div class="w-full max-w-3xl mx-auto">
-      <v-chart :option="option" autoresize style="height: 400px" />
+    <div class="flex space-x-4">
+      <button
+        v-for="option in options"
+        :key="option"
+        @click="selected = option"
+        :class="selected === option ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'"
+        class="px-4 py-2 rounded-md transition hover:cursor-pointer"
+      >
+        {{ option }}
+      </button>
+    </div>
+    <div>
+      <hr class="border-t border-gray-300 my-5" />
+    </div>
+
+    <div
+      class="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden border border-gray-300 p-4"
+    >
+      <h2 class="text-xl font-bold text-gray-800">Stock Information</h2>
+      <ul>
+        <li v-for="(value, key) in meta_data.value" :key="key" class="text-gray-600">
+          <strong>{{ key }}:</strong> {{ value }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="w-full mx-auto">
+      <v-chart :option="option" autoresize style="height: 400px; width: 80vw" />
     </div>
   </main>
 </template>
--- src/components/StocksSymbols.vue
+++ src/components/StocksSymbols.vue
@@ -27,18 +27,20 @@ const handleClick = (event) => {
 
 <template>
   <h2 class="text-lg font-bold mb-4">Stock Symbols</h2>
-  <div class="max-h-[80%] overflow-y-auto border p-4">
-    <ul class="divide-y divide-gray-200">
-      <li
-        @click="handleClick"
-        v-for="symbol in state.symbols"
-        :key="symbol.Symbol"
-        class="py-2 pl-5 odd:bg-gray-300 hover:bg-gray-800 hover:cursor-pointer hover:text-white"
-      >
-        <div class="flex justify-between">
-          <span class="font-medium">{{ symbol.Symbol }}</span>
-        </div>
-      </li>
-    </ul>
+  <div class="max-h-[80vh] overflow-y-auto border p-4">
+    <div>
+      <ul class="divide-y divide-gray-200">
+        <li
+          @click="handleClick"
+          v-for="symbol in state.symbols"
+          :key="symbol.Symbol"
+          class="py-2 pl-5 odd:bg-gray-300 hover:bg-gray-800 hover:cursor-pointer hover:text-white"
+        >
+          <div class="flex justify-between">
+            <span class="font-medium">{{ symbol.Symbol }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
